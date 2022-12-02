50bd46a0fe7963aa7c98b07e42fb7510cd77beff
Subneutral <119481236+Subneutral@users.noreply.github.com>
2022-12-02 09:29:28 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+export default {
+    "images": [
+
+        {
+            "src": "/assets/img/subi.png",
+            "alt": "Image 1"
+        },
+
+    ],
+
+    "domain": ""
+}
+
+
+
+// subneutral.net
